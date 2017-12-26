7b648f7295a815bf6bfff330d2f7b07f42ad1f62
dmitmel <dmytro.meleshko@gmail.com>
2017-12-26 19:06:23 +0200

    fix code style in renderAppHot.js

--- src/renderAppHot.jsx
+++ src/renderAppHot.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { AppContainer } from 'react-hot-loader';
 import { MuiThemeProvider } from 'material-ui/styles';
 import theme from './theme';
