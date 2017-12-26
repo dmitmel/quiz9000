9bd1280eef398cbbaa7bc59b185acece9450d5d9
dmitmel <dmytro.meleshko@gmail.com>
2017-12-26 19:07:47 +0200

    fix type definitions for setState.js

--- src/utils/setState.d.ts
+++ src/utils/setState.d.ts
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 
-export default function setState<K extends keyof S>(
+export default function setState<P, S, K extends keyof S>(
   self: Component<P, S>,
   state: Pick<S, K> | ((prevState: S, props: P) => Pick<S, K>)
 ): Promise<void>;
