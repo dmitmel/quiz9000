7cc59b390da6ab91dc8e1a930bab4fe1ce271bad
dmitmel <dmytro.meleshko@gmail.com>
2017-12-26 19:05:39 +0200

    fix code style in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import registerServiceWorker from './registerServiceWorker';
 import 'normalize.css';
+import registerServiceWorker from './registerServiceWorker';
 import './resets.css';
 import renderApp from './renderApp';
 
