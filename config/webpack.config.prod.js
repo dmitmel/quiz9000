5ae2c8053932d10fd6f98f4357ad3e159b405aec
dmitmel <dmytro.meleshko@gmail.com>
2017-12-27 10:14:39 +0200

    config: optimize webpack config for the development

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -59,10 +59,7 @@ module.exports = {
     // We placed these paths second because we want `node_modules` to "win"
     // if there are any conflicts. This matches Node resolution mechanism.
     // https://github.com/facebookincubator/create-react-app/issues/253
-    modules: ['node_modules', paths.appNodeModules].concat(
-      // It is guaranteed to exist because we tweak it in `env.js`
-      process.env.NODE_PATH.split(path.delimiter).filter(Boolean)
-    ),
+    modules: ['node_modules', paths.appNodeModules],
     // These are the reasonable defaults supported by the Node ecosystem.
     // We also include JSX as a common component filename extension to support
     // some tools, although we do not recommend using it, see:
