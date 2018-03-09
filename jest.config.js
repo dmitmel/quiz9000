e591d7e1bdf117bd00c6b5f568d40129dd3ea2de
dmitmel <dmytro.meleshko@gmail.com>
2018-03-09 12:13:28 +0200

    move Babel and Jest config from package.json to separate files

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx}',
+    '!src/index.js',
+    '!src/registerServiceWorker.js'
+  ],
+  setupFiles: [
+    '<rootDir>/config/polyfills.js',
+    'raf/polyfill',
+    '<rootDir>/config/jest/enzyme.js'
+  ],
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.js?(x)',
+    '<rootDir>/src/**/?(*.)(spec|test).js?(x)'
+  ],
+  testURL: 'http://localhost',
+  transform: {
+    '^.+\\.jsx?$': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
+    '^(?!.*\\.(jsx?|css|json)$)': '<rootDir>/config/jest/fileTransform.js'
+  },
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.jsx?$'],
+  moduleFileExtensions: ['web.js', 'js', 'json', 'web.jsx', 'jsx', 'node'],
+  coverageDirectory: 'coverage'
+};
