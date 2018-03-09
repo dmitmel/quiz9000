d2f568f7d06da66a58e0c63730084a29443e3c2d
dmitmel <dmytro.meleshko@gmail.com>
2018-03-09 14:01:22 +0200

    replace 'promise' module with 'es6-promise'

--- src/db/firebase.js
+++ src/db/firebase.js
@@ -1,14 +1,10 @@
-import '@firebase/polyfill';
+import '@firebase/polyfill/dist/cjs/src/shims/Array';
+import '@firebase/polyfill/dist/cjs/src/shims/String';
 import firebase from '@firebase/app';
 import '@firebase/database';
 
 const config = {
-  apiKey: 'AIzaSyBJE8KocVC4jDAoSb6AeUYaD3Z6qL2kMEc',
-  authDomain: 'quiz9000-db.firebaseapp.com',
-  databaseURL: 'https://quiz9000-db.firebaseio.com',
-  projectId: 'quiz9000-db',
-  storageBucket: '',
-  messagingSenderId: '422403566378'
+  databaseURL: 'https://quiz9000-db.firebaseio.com'
 };
 
 firebase.initializeApp(config);
