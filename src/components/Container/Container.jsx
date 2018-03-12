0c1f381f203d4d1a3fd8686b37e0b116c6e0c35b
dmitmel <dmytro.meleshko@gmail.com>
2018-03-12 07:35:40 +0200

    add Container component

---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import withStyles from 'material-ui/styles/withStyles';
+import NavDrawer from '../NavDrawer';
+
+const navWidth = NavDrawer.width;
+const styles = ({ breakpoints }) => ({
+  container: {
+    margin: '0 auto',
+    [breakpoints.up('sm')]: { maxWidth: breakpoints.values.sm },
+    [breakpoints.up('md')]: { maxWidth: breakpoints.values.md - navWidth },
+    [breakpoints.up('lg')]: { maxWidth: breakpoints.values.lg - navWidth },
+    [breakpoints.up('xl')]: { maxWidth: breakpoints.values.xl - navWidth }
+  }
+});
+
+function Container({ children, classes }) {
+  return <div className={classes.container}>{children}</div>;
+}
+
+Container.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Container);
--- src/components/Page/PageContent.jsx
+++ src/components/Page/PageContent.jsx
@@ -1,25 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import withStyles from 'material-ui/styles/withStyles';
+import Container from '../Container';
 
 const styles = theme => ({
   contentWrapper: {
     width: '100%'
   },
   content: {
-    height: 'calc(100vh - 56px)',
-    marginTop: 56,
-    padding: theme.spacing.unit * 2,
     position: 'relative',
     backgroundColor: theme.palette.background.default,
     overflow: 'auto',
+
+    height: 'calc(100vh - 56px)',
+    marginTop: 56,
     '@media (orientation: landscape)': {
       height: 'calc(100vh - 48px)',
       marginTop: 48
     },
     [theme.breakpoints.up('sm')]: {
       height: 'calc(100vh - 64px)',
-      marginTop: 64,
+      marginTop: 64
+    }
+  },
+  padding: {
+    padding: theme.spacing.unit * 2,
+    [theme.breakpoints.up('sm')]: {
       padding: theme.spacing.unit * 3
     }
   }
@@ -32,7 +38,11 @@ PageContent.propTypes = {
 function PageContent({ classes, children }) {
   return (
     <div className={classes.contentWrapper}>
-      <main className={classes.content}>{children}</main>
+      <main className={classes.content}>
+        <Container classes={{ container: classes.padding }}>
+          {children}
+        </Container>
+      </main>
     </div>
   );
 }
