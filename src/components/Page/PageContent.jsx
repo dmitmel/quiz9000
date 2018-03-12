4c12e12ede8ec93f68318c53fb7e6e6c8fa9b17e
dmitmel <dmytro.meleshko@gmail.com>
2018-03-12 20:50:34 +0200

    page content: remove wrapper div

--- src/components/Page/PageContent.jsx
+++ src/components/Page/PageContent.jsx
@@ -3,25 +3,21 @@ import PropTypes from 'prop-types';
 import withStyles from 'material-ui/styles/withStyles';
 import Container from '../Container';
 
+const contentMarginTop = value => ({
+  height: `calc(100vh - ${value}px)`,
+  marginTop: value
+});
+
 const styles = theme => ({
-  contentWrapper: {
-    width: '100%'
-  },
   content: {
+    width: '100%',
     position: 'relative',
     backgroundColor: theme.palette.background.default,
     overflow: 'auto',
 
-    height: 'calc(100vh - 56px)',
-    marginTop: 56,
-    '@media (orientation: landscape)': {
-      height: 'calc(100vh - 48px)',
-      marginTop: 48
-    },
-    [theme.breakpoints.up('sm')]: {
-      height: 'calc(100vh - 64px)',
-      marginTop: 64
-    }
+    ...contentMarginTop(56),
+    '@media (orientation: landscape)': contentMarginTop(48),
+    [theme.breakpoints.up('sm')]: contentMarginTop(64)
   },
   padding: {
     padding: theme.spacing.unit * 2,
@@ -37,13 +33,9 @@ PageContent.propTypes = {
 
 function PageContent({ classes, children }) {
   return (
-    <div className={classes.contentWrapper}>
-      <main className={classes.content}>
-        <Container classes={{ container: classes.padding }}>
-          {children}
-        </Container>
-      </main>
-    </div>
+    <main className={classes.content}>
+      <Container classes={{ container: classes.padding }}>{children}</Container>
+    </main>
   );
 }
 
