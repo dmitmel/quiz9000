40e243345c12a9513db87e2fc4b2cc89a14f2796
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2018-03-17 20:06:10 +0200

    [actions] add tests

---
+++ src/actions/index.test.js
@@ -0,0 +1,17 @@
+import fs from 'fs-extra';
+import * as index from './index';
+
+describe('actions', () => {
+  describe('it re-exports everything from', () => {
+    fs
+      .readdirSync(__dirname)
+      .filter(filename => !/(^index|\.test)\.js$/.test(filename))
+      .forEach(filename =>
+        test(filename, () => {
+          // eslint-disable-next-line import/no-dynamic-require
+          const module = require(`./${filename}`);
+          expect(index).toMatchObject(module);
+        })
+      );
+  });
+});
