6598201ecd717ea555a04c42b71d46f33c342e96
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2018-03-18 21:05:42 +0200

    [DB] [Firebase] add missing tests

--- src/db/firebase.test.js
+++ src/db/firebase.test.js
@@ -1,21 +1,48 @@
 describe('db/firebase', () => {
   beforeEach(() => jest.resetModules());
 
-  function mockWithFirebaseApp(firebaseApp) {
-    jest.doMock('@firebase/app', () => firebaseApp);
+  function mockWithFirebaseApp(mockFirebaseApp) {
+    jest.doMock('@firebase/app', () => mockFirebaseApp);
     jest.doMock('@firebase/database', () => ({}));
     return require('./firebase');
   }
 
   it('initializes app correctly', () => {
     // given:
-    const initializeApp = jest.fn();
+    const mockInitializeApp = jest.fn();
     // when:
-    mockWithFirebaseApp({ initializeApp, database: () => {} });
+    mockWithFirebaseApp({
+      initializeApp: mockInitializeApp,
+      database: () => {}
+    });
     // then:
-    expect(initializeApp).toHaveBeenCalledTimes(1);
-    expect(initializeApp).toHaveBeenCalledWith({
+    expect(mockInitializeApp).toHaveBeenCalledTimes(1);
+    expect(mockInitializeApp).toHaveBeenCalledWith({
       databaseURL: expect.any(String)
     });
+  });
+
+  it('exports Firebase app', () => {
+    // given:
+    const fakeFirebaseApp = {
+      initializeApp: () => {},
+      database: () => {}
+    };
+    // when:
+    const app = mockWithFirebaseApp(fakeFirebaseApp);
+    // then:
+    expect(app.default).toBe(fakeFirebaseApp);
+  });
+
+  it('exports Firebase database', () => {
+    // given:
+    const fakeFirebaseDatabase = { foo: 'bar' };
+    // when:
+    const { database } = mockWithFirebaseApp({
+      initializeApp: () => {},
+      database: () => fakeFirebaseDatabase
+    });
+    // then:
+    expect(database).toBe(fakeFirebaseDatabase);
   });
 });
