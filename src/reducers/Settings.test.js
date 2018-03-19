ca1e625e7574df68ead18d851856b03b11273a72
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2018-03-19 21:34:51 +0200

    [reducers] [Settings] add tests

---
+++ src/reducers/Settings.test.js
@@ -0,0 +1,37 @@
+import reducer from './Settings';
+import * as actions from '../actions/Settings';
+
+describe('reducers/Settings', () => {
+  it('initializes empty state', () => {
+    // given:
+    const action = { type: 'INIT' };
+    // when:
+    const nextState = reducer(undefined, action);
+    // then:
+    expect(nextState).toEqual({ open: false });
+  });
+
+  describe('+ OPEN_SETTINGS', () => {
+    it('opens the nav if it is closed', () => {
+      // given:
+      const state = { open: false };
+      const action = { type: actions.OPEN_SETTINGS };
+      // when:
+      const nextState = reducer(state, action);
+      // then:
+      expect(nextState).toEqual({ open: true });
+    });
+  });
+
+  describe('+ CLOSE_SETTINGS', () => {
+    it('closes the nav if it is opened', () => {
+      // given:
+      const state = { open: true };
+      const action = { type: actions.CLOSE_SETTINGS };
+      // when:
+      const nextState = reducer(state, action);
+      // then:
+      expect(nextState).toEqual({ open: false });
+    });
+  });
+});
