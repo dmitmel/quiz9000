c4a0660dacd35dcc6428fe4b78c162feca6c6f92
dmitmel <dmytro.meleshko@gmail.com>
2018-03-30 15:01:04 +0300

    [Library] show added quizzes

--- src/components/Library/Library.jsx
+++ src/components/Library/Library.jsx
@@ -2,18 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Page, { PageContent } from '../Page';
 import MainAppBar from '../../containers/MainAppBar';
+import QuizList from '../QuizList';
 import UploadQuiz from './UploadQuiz';
 
-const Library = ({ addQuiz }) => (
+const Library = ({ quizzes, addQuiz }) => (
   <Page>
     <MainAppBar title="Library" />
     <PageContent>
+      <QuizList quizzes={quizzes} />
       <UploadQuiz addQuiz={addQuiz} />
     </PageContent>
   </Page>
 );
 
 Library.propTypes = {
+  quizzes: PropTypes.array.isRequired,
   addQuiz: PropTypes.func.isRequired
 };
 
--- src/containers/Library.js
+++ src/containers/Library.js
@@ -2,6 +2,14 @@ import { connect } from 'react-redux';
 import * as actions from '../actions/creators';
 import Library from '../components/Library';
 
-export default connect(null, dispatch => ({
-  addQuiz: (id, data) => dispatch(actions.addQuiz(id, data))
-}))(Library);
+export default connect(
+  state => ({
+    quizzes: Object.keys(state.Library).map(id => {
+      const data = state.Library[id];
+      return { ...data, id };
+    })
+  }),
+  dispatch => ({
+    addQuiz: (id, data) => dispatch(actions.addQuiz(id, data))
+  })
+)(Library);
